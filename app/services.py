ee0805fc217b9c469894fae2fa411786c7b4d3db
Harshak777 <harshakkrishnaa777@gmail.com>
2024-03-26 15:58:23 -0700

    Added Huey services

---
+++ app/services.py
@@ -0,0 +1,31 @@
+from huey import RedisHuey
+from redis import Redis
+
+huey = RedisHuey("queue", host="localhost")
+r = Redis(host="localhost", port=6379)
+
+class Services:
+    def __init__(self, host="localhost"):
+        self.huey_client = RedisHuey(host)
+
+    @huey.task()
+    def set_value(key, value):
+        print(key, value)
+        response = r.set(key, value)
+        return response
+
+    @huey.task()
+    def get_value(key):
+        if r.exists(key):
+            response = r.get(key)
+            print(response)
+            return response
+        else:
+            return "Key not found"
+
+
+    @huey.task()
+    def delete_key(key):
+        response = r.delete(key)
+        print(response)
+        return "Key deleted"
